e01419f105d110319316d2860a9f48455402f3a4
Dmitry Ilin <dm-ilin@mail.ru>
2022-08-18 17:16:15 +0400

    Сверстать секцию технологии

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,29 @@
+import './AboutProject.css';
+
+function AboutProject() {
+  return (
+    <section className='aboutproject'>
+      <div className='page__container aboutproject__container'>
+        <h4 className='aboutproject__section-name page__section-name'>О проекте</h4>
+        <div className="aboutproject__features">
+          <div className='aboutproject__feature'>
+            <h4 className='aboutproject__feature-heading'>Дипломный проект включал 5 этапов</h4>
+            <p className='aboutproject__feature-text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          </div>
+          <div className='aboutproject__feature'>
+            <h4 className='aboutproject__feature-heading'>На выполнение диплома ушло 5 недель</h4>
+            <p className='aboutproject__feature-text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          </div>
+        </div>
+        <div className='aboutproject__feature-bar'>
+            <div className='aboutproject__bar-cell aboutproject__bar-cell_1'>1 неделя</div>
+            <div className='aboutproject__bar-cell aboutproject__bar-cell_2'>4 недели</div>
+            <div className='aboutproject__bar-cell aboutproject__bar-cell_3'>Back-end</div>
+            <div className='aboutproject__bar-cell aboutproject__bar-cell_4'>Front-end</div>
+          </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutProject;
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -2,7 +2,22 @@ import './Techs.css';
 
 function Techs() {
     return (
-        <h2>Блок Techs</h2>
+        <section className='techs'>
+          <div className='techs__container page__container'>
+            <h4 className='techs__section-name page__section-name'>Технологии</h4>
+            <h3 className='techs__section-header page__section-header'>7 технологий</h3>
+            <p className='techs__section-text page__section-text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+            <ul className='techs__elements'>
+              <li className='techs__element'>HTML</li>
+              <li className='techs__element'>CSS</li>
+              <li className='techs__element'>JS</li>
+              <li className='techs__element'>React</li>
+              <li className='techs__element'>Git</li>
+              <li className='techs__element'>Express.js</li>
+              <li className='techs__element'>mongoDB</li>
+            </ul>
+          </div>
+        </section>
     )
 }
 
